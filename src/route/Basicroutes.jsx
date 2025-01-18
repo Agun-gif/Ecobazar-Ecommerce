28fb9e7f391d253a2986525c686b5fdec292caaf
shuvoagun <shuvoagun898@gmail.com>
2025-01-18 15:35:53 +0600

    vercel json setup

--- src/route/Basicroutes.jsx
+++ src/route/Basicroutes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/client/homePage/Home";
 import Shop from "../pages/client/shop/Shop";
 import ShopPing from "../pages/client/shoppingCart/Shopping";
-import Wishlist from "../pages/client/wishList/wishlist";
+import Wishlist from "../pages/client/wishList/Wishlist";
 import MainLayout from "../layouts/MainLayout";
 
 
